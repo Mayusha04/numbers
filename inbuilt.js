8442c2f16baa290230380f9df763387d2a6b0d74
mayusha <mayushachowdary95@gmail.com>
2021-12-13 20:49:33 +0530

    hi

---
+++ inbuilt.js
@@ -0,0 +1,23 @@
+// .toString()
+var num=20;
+console.log(typeof(num))
+console.log(num.toString())
+console.log(typeof(num.toString()))
+//to convert string to number-parseInt()
+var str='20';
+console.log(typeof(str))
+console.log(parseInt(str))
+console.log(typeof(parseInt(str)))
+
+//parsefloat() *parses a string and returns a floating point number
+var strNum = "12.342";
+console.log(strNum);
+console.log(typeof(strNum));
+console.log(parseFloat(strNum));
+
+//tofixed takes floating point number and stays at a particular position
+var num=12.456732;
+console.log(num)
+console.log(num.toFixed(12))
+console.log(typeof(num))
+
